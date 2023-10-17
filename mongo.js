b69df15fcdb779cd9f97a922369311396ebbab1f
Keranod <konrad.konkel@wp.pl>
2023-10-17 15:29:59 +0100

    3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://konradkonkel:${password}@phonebook.2tsnwwz.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  personName = process.argv[3];
+  personNumber = process.argv[4];
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
