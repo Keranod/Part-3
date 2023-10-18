9c83ad40314af72ca82f02c0d109904117a31072
Keranod <konrad.konkel@wp.pl>
2023-10-18 14:07:20 +0100

    3.13 take 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
+const Person = require("./models/person");
 
 const morgan = require("morgan");
 
@@ -25,36 +27,15 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
